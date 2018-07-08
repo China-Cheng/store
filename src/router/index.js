2f8ca69e861d4e00d9553d964e8191d2851b371a
name-WuCheng <1014967685qq@.com>
2018-07-08 10:36:54 +0800

    登录框完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Login from '@/views/Login';
 
-Vue.use(Router)
-;(() => {
-
-})();
+Vue.use(Router);
 export default new Router({
   routes: [
     {
+      name: 'login',
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'login',
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Login
     }
   ]
 });
