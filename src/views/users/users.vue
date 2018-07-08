f4b6c5c45237ed107a8f4c644eb26739d28e50bf
name-WuCheng <1014967685qq@.com>
2018-07-08 20:21:13 +0800

    搜索框

--- src/views/users/users.vue
+++ src/views/users/users.vue
@@ -1,5 +1,6 @@
 <template>
   <el-card class="box-card">
+    <!-- 面包屑 -->
     <el-breadcrumb separator="/">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>
@@ -7,6 +8,16 @@
       </el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <!-- 搜索框 -->
+    <el-row class="searchArea">
+      <el-col :span="24">
+        <el-input class="searchInput" clearable placeholder="请输入内容">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
  </el-card>
 </template>
 
@@ -19,5 +30,13 @@ export default {
 <style>
 .box-card {
   height: 100%;
+}
+
+.searchArea {
+  margin: 10px 0;
+}
+
+.searchArea .searchInput {
+  width: 350px;
 }
 </style>
