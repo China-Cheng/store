5980375b96e3394dfb1660269580d8d083428a16
name-WuCheng <1014967685qq@.com>
2018-07-08 14:19:47 +0800

    登录代码没有回调

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,7 +12,7 @@
       <el-form-item label="密码">
         <el-input type="password" v-model="fromData.password"></el-input>
       </el-form-item>
-      <el-button class="login-btn" type="primary">登录</el-button>
+      <el-button class="login-btn" type="primary" @click="handleLogin">登录</el-button>
     </el-form>
   </div>
 </template>
@@ -27,6 +27,43 @@ export default {
         password: ''
       }
     };
+  },
+  methods: {
+    // 点击按钮登陆
+    async handleLogin() {
+      const res = await this.$http.post('login', this.fromData);
+      const data = res.data;
+      console.log(data);
+      const { meta: { status, msg } } = data;
+      if (status === 200) {
+        this.$message.success(msg);
+        const { data: { token } } = data;
+        sessionStorage.setItem('token', token);
+      } else {
+        this.$message.error(msg);
+      }
+    // handleLogin() {
+    //   this.$http
+    //     .post('login', this.fromData)
+    //     .then((res) => {
+    //       console.log(res);
+    //       // {data:{},meta:{status:200,msg:""}}
+    //       // const { data: { data: { token }, meta: {status, msg} }}
+    //       const data = res.data;
+    //       const { meta: { status, msg } } = data;
+    //       if (status === 200) {
+    //         // 登陆成功
+    //         const token = data.data.token;
+    //         // 跳转到后台页面
+    //         // 记录token,seetionStotage
+    //         sessionStorage.setItem('token', token);
+    //         this.$message.success(msg);
+    //       } else {
+    //         // 失败
+    //         this.$message.error(msg);
+    //       }
+    //     });
+    }
   }
 };
 </script>
